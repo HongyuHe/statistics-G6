20f70de45d4f1720c07211c6bdc19289a5ece619
Bruno Hoevelaken <43100218+vrij@users.noreply.github.com>
2019-11-01 15:01:50 +0100

    Update Assignment 0.7.R

--- a0/Assignment 0.7.R
+++ a0/Assignment 0.7.R
@@ -2,21 +2,27 @@ a07 = function(x){
   (x - 2)^2
 }
 
-b07 = function(x){
-  sample(x, 50, TRUE)
-  red = sum(x<=30) / 50
-  white = sum(x>30) / 50
+b07 = function(){
+  takenSample = sample(1:100, 50, TRUE)
+  nrPerColour = numeric(2)
+  nrPerColour = c(sum(takenSample <= 30) / 50,
+                  sum(takenSample >  30) / 50)
+  nrPerColour
 }
 
-c07 = function(x, n){
-  sample(x, n, TRUE)
-  fraction = c(sum(x<=mr) / n, sum(x>wr) / n)
-  fraction
+c07 = function(n = 10, mr = 30, mw = 70){
+  totalNrBalls   = mr+mw
+  takenSample    = sample(1:totalNrBalls, n, TRUE)
+  fractionVector = numeric(2)
+  fractionVector = c(sum(takenSample <= mr) / n, 
+                     sum(takenSample >  mr) / n)
+  fractionVector
 }
 
 d07 = function(){
-  u = length(200)
+  u = numeric(200)
   for(i in 1:200) {
     u[i] = median(sample(1:90, 40, TRUE))
   }
+  u
 }
